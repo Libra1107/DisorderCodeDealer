94c5f31d90fb4127c9f7a56da6a311a4493dc142
Libra1107 <11165861+shanra_1107@user.noreply.gitee.com>
2023-05-12 15:33:54 +0000

    0.1.1
    Signed-off-by: Libra1107 <11165861+shanra_1107@user.noreply.gitee.com>

---
+++ mainwindow.cpp
@@ -0,0 +1,94 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <QDebug>
+
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    model = new QFileSystemModel(this);
+    model->setRootPath(QDir::currentPath());
+    ui->fileBrower->setModel(model);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    QString inputString = ui->sourceCode->toPlainText();
+    if(inputString==0)
+    {
+        QMessageBox::warning(nullptr,tr("提示"),tr("输入内容"));
+        return;
+    }
+    ui->solvedCode->clear();
+    QTextCodec* fromcodec = QTextCodec::codecForName("GBK");
+    QByteArray getstring = fromcodec->fromUnicode(inputString);
+
+    QTextCodec* tocodec = QTextCodec::codecForName("Shift-JIS");
+
+    QString outputString = tocodec->toUnicode(getstring);
+    ui->solvedCode->insertPlainText(outputString);
+}
+
+
+void MainWindow::on_fileBrower_clicked(const QModelIndex &index)
+{
+    QString filePath = model->filePath(index);
+    ui->filePath->setText(filePath);
+}
+
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    QString filePath = ui->filePath->text();
+    if (!filePath.endsWith(".txt", Qt::CaseInsensitive)) {
+        QMessageBox::warning(nullptr,tr("抱歉"),tr("暂不支持非文本类文件"));
+        return;
+    }
+
+    QFile readFile(filePath);
+    if (!readFile.open(QIODevice::Text | QIODevice::ReadOnly)) {
+        QMessageBox::warning(nullptr,tr("错误"),tr("无法打开文件"));
+        return;
+    }
+
+    QTextStream readStream(&readFile);
+    QTextCodec *readCodec = QTextCodec::codecForName("GBK");
+    readStream.setCodec(readCodec);
+
+    QFile writeFile(filePath + ".trans");
+    if (!writeFile.open(QIODevice::Text | QIODevice::WriteOnly)) {
+        QMessageBox::warning(nullptr,tr("错误"),tr("无法写入"));
+        return;
+    }
+
+    QTextStream writeStream(&writeFile);
+    QTextCodec *transCodec = QTextCodec::codecForName("Shift-Jis");
+    QTextCodec *writeCodec = QTextCodec::codecForName("UTF-8");
+    writeStream.setCodec(writeCodec);
+
+    while (!readStream.atEnd()) {
+        QString line = readStream.readLine();
+        QByteArray getLine = readCodec->fromUnicode(line);
+        QString outputString = transCodec->toUnicode(getLine);
+        qDebug()<<line;
+        qDebug()<<outputString;
+        writeStream<<outputString<<'\n';
+    }
+
+    readFile.close();
+    readFile.remove();
+    writeFile.close();
+
+    QFile::rename(filePath + ".trans", filePath);
+
+}
+
